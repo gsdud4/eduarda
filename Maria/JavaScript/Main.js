09e1d9f0d61cf64538bd3fe9545e8ea9c5f0776b
gsdud4 <116004557+gsdud4@users.noreply.github.com>
2022-11-23 13:49:10 -0300

    Add files via upload

---
+++ Maria/JavaScript/Main.js
@@ -0,0 +1,40 @@
+console.log("Juliana");
+console.log(123456);
+
+var nome = "juliana"
+let idade = 22;
+
+const pi = 3.14;
+
+console.log(nome);
+console.log(idade);
+console.log(pi);
+
+idade += 1;
+console.log(idade);
+
+/*************************/
+let x = 3;
+if (x > 0) {  //estrutura condicional
+     console.log("OI")
+}
+
+let y = 2;
+while (y < 5) {//laço de repetiçao enquanto
+    console.log(y);
+    y += 1;
+}
+
+let z = 1;
+for (var i = 0; i<10; i++) {//laço de repetiçao para
+    console.log (z);
+    z += 45;
+}
+
+
+/**********************************/
+let xy = [3, 5, 7, 7, 9,]
+console.log(xy);
+console.log(xy[2]);
+/*************************/
+alert("Bem vindo ao site");
